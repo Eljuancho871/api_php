9ccd7aa955c23b185822c3e3dbd0234c9c293712
Juan Diego <juandiegoramirez098@gmail.com>
2023-06-01 14:57:42 -0500

    fix: :bug: Bug rutas no importado

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -3,13 +3,13 @@
 $url = array_filter(explode("/", $_SERVER["REQUEST_URI"]));
 $id = isset($url[5]) ? $url[5] : false;
 
-if ($url[4] == "personas"){
+if ($url[4] === "personas"){
 
     require_once "./app/controllers/Personas_controllers.php";
 
 }else{
 
-    exit("EndPoint no encontrado:(");
+    exit("end point no encontrado:(");
 }
 
 
--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once "./app/controllers/Personas_controllers.php";
+
+require_once "./app/routes/routes.php";
